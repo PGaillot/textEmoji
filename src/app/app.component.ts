c33ce786053ad375e100146daafeda89b0c1d1b5
PGaillot <pr.gaillot@gmail.com>
2024-05-16 19:15:30 +0200

    add detect words

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,11 @@ import { Component } from '@angular/core';
 import { FormControl, ReactiveFormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
+interface MagicWords {
+  words: string[];
+  emoji: string;
+}
+
 @Component({
   selector: 'app-root',
   standalone: true,
@@ -10,50 +15,108 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'textEmoji';
+  magicsWords: MagicWords[] = [
+    {
+      words: ['poule', 'poules', 'poulette', 'poulettes', 'poupoule', 'poupoules'],
+      emoji: ' :chicken: ',
+    },
+    {
+      words: ['lunette', 'lunettes'],
+      emoji: ' :eyeglasses: ',
+    },
+    {
+      words: ['<3', 'love', 'coeur', 'cœur'],
+      emoji: ' :purple_heart: ',
+    },
+  ];
+
   result: string = '';
-
   letterEmoji: string = ' :regional_indicator_{letter}: ';
-
   inputText: FormControl = new FormControl('');
 
+  copy() {
+    navigator.clipboard.writeText(this.result);
+  }
 
+  convertTextToEmojis(text: string): string {
+    let result: string = '';
 
-  copy(){
-  navigator.clipboard.writeText(this.result);
+    const inputArrray: string[] = text.split('');
+
+    inputArrray.forEach((letter: string) => {
+      let emoji: string = '';
+
+      switch (letter) {
+        case ' ':
+          emoji = ' :blue_square: ';
+          break;
+
+        case "'":
+          emoji = ' :blue_square: ';
+          break;
+
+        case '?':
+          emoji = ' :question: ';
+          break;
+
+        case '!':
+          emoji = ' :exclamation: ';
+          break;
+
+        default:
+          emoji = this.letterEmoji.replace('{letter}', letter.toLowerCase());
+          break;
+      }
+      result += emoji;
+    });
+
+    return result;
   }
 
   ngOnInit(): void {
+    const magicsTriggersWords: string[] = this.magicsWords.reduce(
+      (allWords: string[], magic: MagicWords) => {
+        return (allWords = [...allWords, ...magic.words]);
+      },
+      []
+    );
+
+    console.log(magicsTriggersWords);
+
     this.inputText.valueChanges.subscribe((input: string) => {
-      this.result = '';
-      const inputArrray: string[] = input.split('');
-      let res: string = '';
-      inputArrray.forEach((letter: string) => {
-        let emoji: string = '';
+      this.result = ''
+      let parts: string[] = [input];
 
-        switch (letter) {
-          case ' ':
-            emoji = ' :blue_square: ';
-            break;
+      if (magicsTriggersWords.some((w: string) => input.includes(w))) {
+        magicsTriggersWords.forEach((w: string) => {
+          if (input.indexOf(w) >= 0) {
+            console.log(input.indexOf(w));
+            const magicWord: MagicWords = this.magicsWords.filter(
+              (mw: MagicWords) => mw.words.includes(w)
+            )[0];
+            parts = [
+              input.slice(0, input.indexOf(w)),
+              magicWord.emoji,
+              input.slice(input.indexOf(w) + w.length, input.length),
+            ];
+            console.log(parts);
+          }
+        });
+      }
 
-            case '\'':
-            emoji = ' :blue_square: ';
-            break;
 
-            case '?':
-            emoji = ' :question: ';
-            break;
+      parts.forEach((part:string) => {
 
-            case '!':
-            emoji = ' :exclamation: ';
-            break;
+        const isMagicEmoji = this.magicsWords.some((mw: MagicWords) => mw.emoji.trim() === part.trim());
 
-          default:
-            emoji = this.letterEmoji.replace('{letter}', letter.toLowerCase());
-            break;
+
+        if(!isMagicEmoji){
+          this.result += this.convertTextToEmojis(part)
+        } else {          
+          this.result += part
         }
-        this.result += emoji;
-      });
+      })
+
     });
   }
 }
