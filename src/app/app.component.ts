1a22de9a3861f014f8acd25c57c7690b5ff4d3eb
PGaillot <pr.gaillot@gmail.com>
2024-05-16 17:38:54 +0200

    init.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,49 @@
 import { Component } from '@angular/core';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ReactiveFormsModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'textEmoji';
+  result: string = '';
+
+  letterEmoji: string = ' :regional_indicator_{letter}: ';
+
+  inputText: FormControl = new FormControl('');
+
+  ngOnInit(): void {
+    this.inputText.valueChanges.subscribe((input: string) => {
+      this.result = '';
+      const inputArrray: string[] = input.split('');
+      let res: string = '';
+      inputArrray.forEach((letter: string) => {
+        let emoji: string = '';
+
+        switch (letter) {
+          case ' ':
+            emoji = ' :blue_square: ';
+            break;
+
+            case '?':
+            emoji = ' :question: ';
+            break;
+
+            case '!':
+            emoji = ' :exclamation: ';
+            break;
+
+          default:
+            emoji = this.letterEmoji.replace('{letter}', letter.toLowerCase());
+            break;
+        }
+        this.result += emoji;
+      });
+    });
+  }
 }
